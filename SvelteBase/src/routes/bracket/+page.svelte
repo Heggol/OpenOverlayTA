d33a71293fe8a3076f5819e4c13932fb293254ea
Heggo <heggo@heggo.dev>
2025-05-16 22:53:29 +0200

    Initial commit
    Added Sub-Routes countdown, intermission and bracket

---
+++ SvelteBase/src/routes/bracket/+page.svelte
@@ -0,0 +1,30 @@
+<div class="container">
+            <iframe 
+                title="ChalloneBracket"
+                src="https://challonge.com/JanuaryJinx25/module"
+                class="bracket"
+                allowTransparency={true}
+                frameBorder="1"
+                scrolling="auto"
+                style="width: 72%; height: 100%; border: none;"
+            ></iframe>
+        </div>
+
+<style>
+    .container {
+        position: relative;
+        width: 1920px;
+        height: 1080px;
+        background: transparent;
+        overflow: hidden;
+        isolation: isolate;
+    }
+    .bracket {
+        -webkit-mask-image: linear-gradient(to right, black 50%, black 60%, transparent 70%);
+        mask-image: linear-gradient(to right, black 50%, black 60%, transparent 70%);
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+    }
+</style>
